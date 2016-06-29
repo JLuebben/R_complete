b04202f0fa1d116438f61c7f20802e9c100e3420
JLuebben <jens.luebben@stud.uni-goettingen.de>
2016-06-29 15:58:56 +0200

    .fcf file can now be read by shelxle.

--- makeMap.py
+++ makeMap.py
@@ -23,12 +23,25 @@ def makeMap(fileNameBase, dir='./', output='Rcomplete.fcf'):
                         break
                     # triple = tuple([int(i) for i in line.split()[:3]])
                     # if triple in flaggedIndices:
+                    line = line.strip().split()
+                    fCalc = '{:7.2f}'.format(float(line[5])**.5).strip()
+                    line[5] = fCalc
+                    line = ' ' + ' '.join(line[:-2])
                     data.append(line)
                 elif first:
-                    data.append(line[:-1])
+                    if '_refln_F_squared_calc' in line:
+                        data.append(' _refln_F_calc')
+                    elif '_refln_d_spacing' in line:
+                        continue
+                    elif '_shelx_refinement_sigma' in line:
+                        pass
+                    elif '_shelx_refln_list_code' in line:
+                        data.append('_shelx_refln_list_code          6')
+                    else:
+                        data.append(line[:-1])
                 if '_shelx_refinement_sigma' in line:
                     read = True
         first = False
     with open(output, 'w') as outFile:
-        outFile.write('\n'.join(data))
+        outFile.write('\n'.join(data)+'\n')
 
